1e48824649e92b539d6d20c99b38fbb11f81f87e
StayCoo1 <84853136+StayCoo1@users.noreply.github.com>
2022-05-04 22:18:32 +0500

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,11 @@
+let user = {
+    'key': 'ad4a0c864a5536eee42b163190aeb3e7',
+    'action': 'balance'
+}
+
+fetch('https://smmpanelus.com/api/v2', {
+    method: 'POST',
+    mode: 'no-cors',
+    headers: {},
+    body: JSON.stringify(user)
+}).then(res => console.log(res))
